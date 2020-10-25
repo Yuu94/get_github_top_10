6e103a1ea152704ecf5b5a8e80409f377906ea66
yuu <yuu.taka94@gmail.com>
2020-10-25 23:11:19 +0900

    自動でブラウザを開くように設定を変更

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/app.ts',
+  devServer: {
+    open: true
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
